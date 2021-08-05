2ad6ea00ad8f988a5e9ae157d4ed5047f7c06dbe
Alex Austin <alexaustin@fastmail.com>
2021-08-05 18:14:06 -0400

    More preprocessing

--- preproc.R
+++ preproc.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(hash)
 library(lubridate)
+library(zoo)
 
 show_files = c("the_big_bang_theory.csv", "ncis.csv", "cis.csv", 
                "desperate_housewives.csv", "friends.csv", "er.csv", "frasier.csv", 
@@ -40,17 +41,31 @@ rm(h, show_files, x, season)
 
 shows <- shows %>% mutate(viewers=sapply(viewers,str_replace_all, 
                                          pattern="\\[\\w+\\]|\\[\\w+", replacement=""))
-
 shows <- shows %>% transform(viewers=as.numeric(viewers))
-
 shows <- shows %>% mutate(air_date=sapply(air_date,mdy))
-
 shows <- shows %>% transform(air_date=as_date(air_date))
+shows <- shows %>% filter(viewers>0|is.na(viewers))
+
+View(shows %>% group_by(show) %>% 
+       summarise(pmva_3 = rollapply(viewers, c(-3,-2,-1), mean, fill="extend")))
+
+length(shows$viewers)
+rollapply(shows$viewers, c(-3,-2,-1), mean, fill="extend", partial=TRUE)
+
+View(shows %>% group_by(show) %>% summarise(n())) 
 
 nas_viewers <- shows %>% filter(is.na(viewers))
-nas_dates <- shows %>% filter(is.na(air_date))
-  
-shows %>% summary()
+
+df_cis <- shows %>% filter(show=="CIS") %>% select(air_date, viewers)
+df_cis <- df_cis %>% mutate(lag = lag(viewers))
+df_lag <- df_cis %>% select(air_date, lag)
+
+z_mva <- rollapply(read.zoo(df_lag), 3, mean, align="right", fill="extend", partial=TRUE)
+
+df_mva = data.frame(air_date=index(z_mva), as.data.frame(z_mva) %>% rename(pmva_3 = z_mva))
+
+df_cis$pmva_3 = df_mva$pmva_3
+
 
 
 
